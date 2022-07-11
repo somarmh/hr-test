113aa62139f61174e43a8e3cb2964eb89eac5132
somarmh <somarabdm@gmail.com>
2022-07-11 17:19:21 +0300

    Final

--- app/routes/user.js
+++ app/routes/user.js
@@ -13,23 +13,9 @@ let router = express.Router();
 
 const dbconnection = require("../config/db_config");
 let connAttr = dbconnection.connAttr;
-/*const findOne= (email,password) => {
-  oracledb.getConnection(connAttr, function(err, connection){
-    let query = "select id from \"USER\" where EMAIL = '"+email+ "' and PASSWORD = '" + password+"'";
-    console.log("Database Connected");
-    console.log(query);
-    connection.execute(query, {},{}, function(err, result){
-        if(err) {
-          console.log(err);
-        }
-        connection.release(function(err){
-          console.log("connection is  releasesd")
-        });
-        console.log(result);
-        return res.json({success: "true", user: result});
-        });
-    });
-}*/
+
+
+//login
 router.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -40,9 +26,9 @@ router.post("/login", async (req, res) => {
       const result = await connection.execute(query);
       console.log(result.rows.length);
       if(result.rows.length != 0){
-        const userDetails = {email : email , password :password};//from database
+        const userDetails = {email : email , password :password};
         try{
-            const token = await jwt.sign(userDetails, config.secretKey, {expiresIn:10000})
+            const token = await jwt.sign(userDetails, config.secretKey, {expiresIn:1000})
             const refreshToken = await jwt.sign(userDetails, config.secretKeyRefresh, {expiresIn:864000})
             return res.json({success:true, accessToken: token, refreshToken: refreshToken});
         } catch(err){
@@ -61,15 +47,31 @@ router.post("/login", async (req, res) => {
   }  
 });
 
+//refresh
+router.post("/refresh", function(req,res) {
+  console.log("so");
+  const refreshtoken=req.body.refreshToken;
+  jwt.verify(refreshtoken, config.secretKeyRefresh, async (err, decoded) =>{
+      if(err){
+          return res.json({success: false, msg: err.message}) 
+      }
+      console.log(decoded) ;
+      const userDetails = {email : decoded.email , password : decoded.password};
+        try{
+            const token = await jwt.sign(userDetails, config.secretKey, {expiresIn:1000})
+            return res.json({success:true, accessToken: token});
+        } catch(err){
+            console.log(err);
+            return  res.json({success:false, msg: err.message})
+        }
+  });
+});
 
 // ...
 //CRUD
 
 //Read
 router.get("/getUsers", auth.verifyToken, async(req, res) =>{
-  const decodedToken = res.locals.decodedToken;
-  const email = decodedToken.email;
-  if(email == "Ghaffar"){
     let connection;
     try{
         connection = await oracledb.getConnection(connAttr);
@@ -83,21 +85,65 @@ router.get("/getUsers", auth.verifyToken, async(req, res) =>{
     }finally{
         await connection.release();
     }
-}
-else{
-    return res.json({success: false, msg: "Unauthorized.."}); 
-}
 });
 
+
 //CREATE
-router.post("/addUser", function (req, res) {
-  oracledb.getConnection(connAttr, function (err, connection) {
-      if (err) {
-          console.log(err);
-      }
-      console.log("Connected to Database");
+router.post("/addUser", auth.verifyToken, async(req, res) =>{
+  console.log("so22mar");
+  let connection;
+    try{
+        connection = await oracledb.getConnection(connAttr);
+        let query =
+        'insert into "USER" (EMAIL , PASSWORD , PHONE , ADDRESS , FIRST_NAME , LAST_NAME) values( :EMAIL , :PASSWORD , :PHONE , :ADDRESS , :FIRST_NAME , :LAST_NAME )';
+          let binds = [
+              req.body.EMAIL,
+              req.body.PASSWORD,
+              req.body.PHONE,
+              req.body.ADDRESS,
+              req.body.FIRST_NAME,
+              req.body.LAST_NAME
+          ];        
+        const result = await connection.execute(query,binds,{ autoCommit: true });
+        console.log(result);
+        return res.json(result.rows);
+    }catch(err){
+        console.log(err);
+        return res.json({success : false, msg: err.message}); 
+    }finally{
+        await connection.release();
+    }
+});
+
+
+//Delete
+router.get("/deleteUser/:id", auth.verifyToken, async(req, res) =>{
+  let connection;
+    try{
+        connection = await oracledb.getConnection(connAttr);
+        let id = req.params.id;
+        console.log(id);
+        let query = 'Delete  from "USER"  where ID = ' + id ;       
+        const result = await connection.execute(query, {}, {autoCommit: true });
+        console.log(result);
+        return res.json(result.rows);
+    }catch(err){
+        console.log(err);
+        return res.json({success : false, msg: err.message}); 
+    }finally{
+        await connection.release();
+    }
+});
+
+
+//Update
+router.get("/updateUser/:id", auth.verifyToken, async(req, res) =>{
+  let connection;
+    try{
+      connection = await oracledb.getConnection(connAttr);
+      let id = req.params.id;
       let query =
-          'insert into "USER" (EMAIL , PASSWORD , PHONE , ADDRESS , FIRST_NAME , LAST_NAME) values( :EMAIL , :PASSWORD , :PHONE , :ADDRESS , :FIRST_NAME , :LAST_NAME )';
+          'Update "USER" set EMAIL = :EMAIL , PASSWORD = :PASSWORD , PHONE = :PHONE , ADDRESS = :ADDRESS , FIRST_NAME = :FIRST_NAME , LAST_NAME = :LAST_NAME where id = '+ id;
       let binds = [
           req.body.EMAIL,
           req.body.PASSWORD,
@@ -106,89 +152,16 @@ router.post("/addUser", function (req, res) {
           req.body.FIRST_NAME,
           req.body.LAST_NAME
       ];
-      connection.execute(
-          query,
-          binds,
-          { autoCommit: true },
-          function (err, result) {
-              console.log("Executing query....");
-              if (err) {
-                  console.log(err);
-              }
-
-              connection.release(function (err) {
-                  console.log("connection is releasesd");
-              });
-              return res.json(result.rows);
-          }
-      );
-  });
-});
-
-//Delete
-
-router.get("/DeleteUser/:id", function (req, res) {
-  oracledb.getConnection(connAttr, function(err, connection){
-    let id = req.params.id;
-    console.log("id=" +id);
-    if(err){
-      console.log(err);
-    }
-    console.log("Connected to Database")
-    let query = 'Delete  from "USER"  where ID = ' + id ;
-    connection.execute(query, {}, {autoCommit: true }, function(err, result){
-      console.log("Executing query...")
-      if(err){
+     
+        const result = await connection.execute(query, binds, {autoCommit: true });
+        console.log(result);
+        return res.json(result.rows);
+    }catch(err){
         console.log(err);
-        return res.json({success: "false", msg: err.message});
-      }
-      connection.release(function(err){
-        console.log("connection is releasesd");
-      });
-      return res.json({success: "true"});
-    });
-
-  });
-
-});
-
-
-//Update
-router.post("/updateUser/:id", function (req, res) {
-  oracledb.getConnection(connAttr, function (err, connection) {
-      if (err) {
-          console.log(err);
-      }
-      console.log("Connected to Database");
-      let id = req.params.id;
-      let query =
-          'Update "USER" set EMAIL = :EMAIL , HASH_PASSWORD = :HASH_PASSWORD , PHONE = :PHONE , ADDRESS = :ADDRESS , FIRST_NAME = :FIRST_NAME , LAST_NAME = :LAST_NAME where id = '+ id;
-
-      let binds = [
-          req.body.EMAIL,
-          bcrypt.hashSync(req.body.PASSWORD, 8),
-          req.body.PHONE,
-          req.body.ADDRESS,
-          req.body.FIRST_NAME,
-          req.body.LAST_NAME
-      ];
-
-      connection.execute(
-          query,
-          binds,
-          { autoCommit: true },
-          function (err, result) {
-              console.log("Executing query....");
-              if (err) {
-                  console.log(err);
-              }
-              connection.release(function (err) {
-                  console.log("connection is releasesd");
-              });
-              return res.json(result.rows);
-          }
-      );
-  });
+        return res.json({success : false, msg: err.message}); 
+    }finally{
+        await connection.release();
+    }
 });
 
 
